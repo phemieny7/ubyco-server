eb15b440e631fde0c2a9ab60201fc3a8199ece7d
Phemieny7 <oyewo.oluwafemi@gmail.com>
2021-09-22 19:36:03 -0700

    worker

--- config/database.ts
+++ config/database.ts
@@ -10,30 +10,10 @@ import { DatabaseConfig } from "@ioc:Adonis/Lucid/Database";
 
 const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
 const databaseConfig: DatabaseConfig = {
-  /*
-  |--------------------------------------------------------------------------
-  | Connection
-  |--------------------------------------------------------------------------
-  |
-  | The primary connection for making database queries across the application
-  | You can use any key from the `connections` object defined in this same
-  | file.
-  |
-  */
   connection: Env.get("DB_CONNECTION"),
 
   connections: {
-    /*
-    |--------------------------------------------------------------------------
-    | PostgreSQL config
-    |--------------------------------------------------------------------------
-    |
-    | Configuration for PostgreSQL database. Make sure to install the driver
-    | from npm when using this connection
-    |
-    | npm i pg
-    |
-    */
+   
     pg: {
       client: "pg",
       connection: {
@@ -41,7 +21,7 @@ const databaseConfig: DatabaseConfig = {
         port: Env.get("PG_PORT", ''),
         user: Env.get("PG_USER", DATABASE_URL.username),
         password: Env.get("PG_PASSWORD", DATABASE_URL.password),
-        database: Env.get("PG_DATABASE", DATABASE_URL.pathname.substr(1)),
+        database: Env.get("PG_DB_NAME", DATABASE_URL.pathname.substr(1)),
         ssl: {
           rejectUnauthorized: false,
         },
